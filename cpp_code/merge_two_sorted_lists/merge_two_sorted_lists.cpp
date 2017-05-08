0851ef887a48685cf62ab7e26aedf4b8af877e31
reedjosh <reedjosh@oregonstate.edu>
2017-05-08 14:09:39 -0700

    solved merge two sorted list problem.

--- cpp_code/merge_two_sorted_lists/merge_two_sorted_lists.cpp
+++ cpp_code/merge_two_sorted_lists/merge_two_sorted_lists.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 struct ListNode {
     int val;
     ListNode *next;    
-    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int x) : val(x), next(NULL) {} // Constructor.
     };
 
 
-void addToList(ListNode* list, int val){
+void addToList(ListNode* list, vector<int> nums){
     // Don't want to lose head, so use this pointer instead.
     ListNode* present = list; 
 
@@ -18,8 +19,11 @@ void addToList(ListNode* list, int val){
         present = present->next;
 
     // Create the new node and append it.
-    ListNode* new_node = new ListNode(val);
-    present->next = new_node;
+    for (auto it : nums){
+        ListNode* new_node = new ListNode(it);
+        present->next = new_node;
+        present = present->next;
+        }
     }
 
 
@@ -36,36 +40,46 @@ void printList(ListNode* list){
     cout << present->val << endl;
     }
 
-ListNode* mergeTwoLists(ListNode* l1, ListNode* l2){
-        ListNode dummy(0);
-        ListNode* 
-
-        While(l1 || l2){
-            l1 = l1 ? l1->next : l1;
-            l2 = l2 ? l2->next : l2;
-
-            if (l1->val <= l2->val) 
-            else 
+class Solution{
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2){
+            ListNode dummy(0);
+            ListNode* conductor = &dummy;
+    
+            while(l1 && l2){ // While there are two lists.
+                if (l1->val < l2->val){  // Pick the smaller number.
+                    conductor->next = l1;
+                    l1 = l1 ? l1->next : l1;
+                    }
+                else{
+                    conductor->next = l2;
+                    l2 = l2 ? l2->next : l2;
+                    }
+                conductor = conductor->next; // Set conductor to tail.
+                }
+    
+            conductor->next = (l1) ? l1 : l2; // Connect conductor to remaining list.
+            return dummy.next;
             }
-    }
+    };
 
 
 int main(){
 
+    Solution solution;
 
-    ListNode* l1 = new ListNode(2);
-    addToList(l1, 4);
-    addToList(l1, 3);
+    ListNode* l1 = new ListNode(1);
+    
+    //addToList(l1, {3});
 
-    ListNode* l2 = new ListNode(5);
-    addToList(l2, 6);
-    addToList(l2, 4);
+    ListNode* l2 = new ListNode(-5);
+    //addToList(l2, {6, 7});
 
     printList(l1);
     printList(l2);
 
 
-    ListNode* result = mergeTwoLists(l1, l2);
+    ListNode* result = solution.mergeTwoLists(l1, l2);
 
     printList(result);
 
